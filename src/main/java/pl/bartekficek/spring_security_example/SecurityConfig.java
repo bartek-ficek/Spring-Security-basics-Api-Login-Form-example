2859b373092da6575b99da04439b130eee8c8017
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-19 21:47:42 +0100

    method configure() added to SecurityConfig

--- src/main/java/pl/bartekficek/spring_security_example/SecurityConfig.java
+++ src/main/java/pl/bartekficek/spring_security_example/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.bartekficek.spring_security_example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,12 +14,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
+        UserDetails user = User.withDefaultPasswordEncoder()
                 .username("user")
                 .password("user1")
                 .roles("USER")
                 .build();
-        return new InMemoryUserDetailsManager(userDetails);
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin1")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/hello").permitAll()
+                .anyRequest().hasRole("ADMIN")
+                .and()
+                .formLogin().permitAll();
+    }
 }
